a1d17bc21fd99a0e4c3bede216e1a9573bb3526f
ruleihui <39218770+ruleihui@users.noreply.github.com>
2020-10-04 13:22:32 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,12 +15,48 @@ rm -rf /tmp/wordpress
 
 # V2Ray new configuration
 install -d /usr/local/etc/wordpress
-curl -fsSL https://raw.githubusercontent.com/ruleihui/gitTest/master/test1.json -o "test.json"
-mv test.json /usr/local/etc/wordpress/test.json
+
+base64 << EOF > /usr/local/etc/wordpress/test
+{
+    "log": {
+        "access": "none",
+        "loglevel": "error"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "/path"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+
 
 # Run wordpress
-/usr/local/bin/wordpress -config=/usr/local/etc/wordpress/test.json &
+/usr/local/bin/wordpress -config=/usr/local/etc/wordpress/test &
 sleep 5s
-rm -f /usr/local/bin/v2ctl /usr/local/etc/wordpress/test.json
+rm -f /usr/local/bin/v2ctl /usr/local/etc/wordpress/test
 history -c
 
